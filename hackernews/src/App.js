7999e14ee9f1df57b1462bf20a3795ab89dbfc73
kirbycampbell <jkirbycampbell@gmail.com>
2018-11-15 10:31:28 -0800

    Restructuring

--- hackernews/src/App.js
+++ hackernews/src/App.js
@@ -1,120 +1,48 @@
 import React, { Component } from "react";
 import "./App.css";
 
-const list = [
-  {
-    title: "React",
-    url: "https://reactjs.org/",
-    author: "Jordan Walke",
-    num_comments: 3,
-    points: 4,
-    objectID: 0
-  },
-  {
-    title: "Redux",
-    url: "https://redux.js.org/",
-    author: "Dan Abramov, Andrew Clark",
-    num_comments: 2,
-    points: 5,
-    objectID: 1
-  }
-];
+const DEFAULT_QUERY = "redux";
 
-/* ES5 js Way of coding "isSearched()"
-function isSearched(searchTerm) {
-  return function(item) {
-    return item.title.toLowerCase().includes(searchTerm.toLowerCase());
-  };
-}
-*/
-//This is the ES6 js Way of doin it.
-const isSearched = searchTerm => item =>
-  item.title.toLowerCase().includes(searchTerm.toLowerCase());
+const PATH_BASE = "https://hn.algolia.com/api/v1";
+const PATH_SEARCH = "/search";
+const PARAM_SEARCH = "query=";
 
 class App extends Component {
   constructor(props) {
     super(props);
+
     this.state = {
-      list,
-      searchTerm: ""
+      result: null,
+      searchTerm: DEFAULT_QUERY
     };
+    this.setSearchTopStories = this.setSearchTopStories.bind(this);
     this.onSearchChange = this.onSearchChange.bind(this);
     this.onDismiss = this.onDismiss.bind(this);
   }
-  onSearchChange(event) {
-    this.setState({ searchTerm: event.target.value });
+
+  setSearchTopStories(result) {
+    this.setState({ result });
   }
-  onDismiss(id) {
-    const isNotId = item => item.objectID !== id;
-    const updatedList = this.state.list.filter(isNotId);
-    this.setState({ list: updatedList });
+
+  componentDidMount() {
+    const { searchTerm } = this.state;
+
+    fetch(`${PATH_BASE}${PATH_SEARCH}?${PARAM_SEARCH}${searchTerm}`)
+      .then(response => response.json())
+      .then(result => this.setSearchTopStories(result))
+      .catch(error => error);
   }
+
   render() {
-    const { searchTerm, list } = this.state;
+    const { searchTerm, result } = this.state;
+
+    if (!result) {
+      return null;
+    }
     return (
       <div className="page">
-        <div className="interactions">
-          <Search value={searchTerm} onChange={this.onSearchChange}>
-            Search
-          </Search>
-        </div>
-        <Table list={list} pattern={searchTerm} onDismiss={this.onDismiss} />
+        <table />
       </div>
-    );
-  }
-}
-
-/*::: IS now refactored to a stateless functional component ::::::
-function Search({ value, onChange, children }) {
-  return (
-    <form>
-      {children} <input type="text" value={value} onChange={onChange} />
-    </form>
-  );
-}
-*/
-//Example of ES6 arrow function implicit return version of above
-const Search = ({ value, onChange, children }) => (
-  <form>
-    {children} <input type="text" value={value} onChange={onChange} />
-  </form>
-);
-//Connects in the table const below for style variable
-const midColumn = {
-  width: "30%"
-};
-
-const Table = ({ list, pattern, onDismiss }) => (
-  <div className="table">
-    {list.filter(isSearched(pattern)).map(item => (
-      <div key={item.objectID} className="table-row">
-        <span style={{ width: "40%" }}>
-          <a href={item.url}>{item.title}</a>
-        </span>
-        <span style={midColumn}>{item.author}</span>
-        <span style={{ width: "10%" }}>{item.num_comments}</span>
-        <span style={{ width: "10%" }}>{item.points}</span>
-        <span style={{ width: "10%" }}>
-          <Button
-            onClick={() => onDismiss(item.objectID)}
-            className="button-inline"
-          >
-            Dismiss
-          </Button>
-        </span>
-      </div>
-    ))}
-  </div>
-);
-
-class Button extends Component {
-  render() {
-    const { onClick, className = "", children } = this.props;
-
-    return (
-      <button onClick={onClick} className={className} type="button">
-        {children}
-      </button>
     );
   }
 }
